da18d10dfaa0e3bf2462fe9a12324e6e808e7a61
Parsifal <zmw@izmw.me>
2019-05-28 16:22:59 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,5 +6,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "HandyJSON"
+    name: "HandyJSON",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "HandyJSON",
+            targets: ["HandyJSON"])
+    ],
+    targets: [
+        .target(
+            name: "HandyJSON",
+            path: "Source")
+    ],
+    swiftLanguageVersions: [.v5]
 )
